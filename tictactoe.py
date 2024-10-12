380d02308110a067f73f734e9da2b186004785af
Aziz <bhsaziz408@gmail.com>
2024-10-12 18:02:06 +0200

    setup

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+import tkinter
+
+player1 = 'X'
+player2 = 'O'
+current_player = player1
+board = [[0,0,0],[0,0,0],[0,0,0]]
+
+
+window = tkinter.Tk()
+window.title('Tic-Tac-Toe')
+window.resizable(0, 0)
+
+window.mainloop()
+
